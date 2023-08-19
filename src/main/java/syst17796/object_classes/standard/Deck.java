9dd8695fa25e003e51f821298468818162c2bb16
vD-Evan <vanduzee@sheridancollege.ca>
2023-08-19 18:28:20 -0400

    Added: Comments (Object Classes)

--- src/main/java/syst17796/object_classes/standard/Deck.java
+++ src/main/java/syst17796/object_classes/standard/Deck.java
@@ -3,13 +3,13 @@ package syst17796.object_classes.standard;
 import java.util.ArrayList;
 import syst17796.abstract_classes.DeckRequirements;
 
-public class Deck extends DeckRequirements<Card> {
-    public Deck() {
+public class Deck extends DeckRequirements<Card> { // standard deck, uses standard cards
+    public Deck() { // creates a new deck,
         theDeck = new ArrayList<Card>();
-
-        buildDeck();
+        buildDeck(); // builds deck from below logic
     }
 
+    // takes each rank for each suit and each suit, adds new card to deck
     public void buildDeck() {
         for (Suit suit : Suit.values()) {
             for (Rank rank : Rank.values()) {
