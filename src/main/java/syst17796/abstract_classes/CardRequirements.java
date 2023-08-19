9fe6c098f7a9ea9956de5a2ff0473f9ae9545e23
vD-Evan <vanduzee@sheridancollege.ca>
2023-08-19 18:19:36 -0400

    Added: Comments (Abstract Classes)

--- src/main/java/syst17796/abstract_classes/CardRequirements.java
+++ src/main/java/syst17796/abstract_classes/CardRequirements.java
@@ -1,6 +1,16 @@
 package syst17796.abstract_classes;
 
-public abstract class CardRequirements {
+public abstract class CardRequirements { // requirements for future card classes
+    /*
+     * Note:
+     * - Assumes every card has a "Suit"
+     * This can be Spades, as in a standard deck, or Blue, as in Uno
+     * - Assumes every card has a "Rank"
+     * This can be King, as in a standard deck, or 3, as in Uno
+     * - Stores suits and ranks as a String for user visibility and as an integer
+     * for program usage
+     */
+
     public abstract String getSuitAsString();
 
     public abstract int getSuitAsInt();
@@ -10,6 +20,9 @@ public abstract class CardRequirements {
     public abstract int getRankAsInt();
 
     // Custom toString method for cards.
+    // Special cards, ie Uno, will override this with a more standard convention
+    // ie "Blue +2" reading out as: 'return getSuitAsString() + " " +
+    // getRankAsString();'
     @Override
     public String toString() {
         return getRankAsString() + " of " + getSuitAsString();
--- src/main/java/syst17796/abstract_classes/DeckRequirements.java
+++ src/main/java/syst17796/abstract_classes/DeckRequirements.java
@@ -3,29 +3,31 @@ package syst17796.abstract_classes;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public abstract class DeckRequirements<T> {
-    protected ArrayList<T> theDeck = new ArrayList<T>();
+public abstract class DeckRequirements<T> { // requirements for future deck classes
+    protected ArrayList<T> theDeck = new ArrayList<T>(); // uses <T> as a type placeholder as decks can be made of
+                                                         // standard or special cards
 
-    public abstract void buildDeck();
+    public abstract void buildDeck(); // Assumes all decks must be be built (ie all cards added to it
 
-    public void shuffle() {
+    public void shuffle() { // uses Collections to shuffle the Array List
         Collections.shuffle(theDeck);
     }
 
-    public T dealCard() {
+    public T dealCard() { // Deals a card, prompts users if there's an error.
         if (!theDeck.isEmpty()) {
             return theDeck.remove(0);
         }
         System.out.println("There are no remaining cards in the deck to deal.");
-        return null;
+        return null; // up to each game to determine how to handle an empty deck (game over,
+                     // reshuffle, etc.)
     }
 
-    public int getSize() {
+    public int getSize() { // if games require the deck size (ie. for scoring purposes)
         return theDeck.size();
     }
 
     @Override
-    public String toString() {
+    public String toString() { // returns the size of the deck
         return "Number of cards remaining in the deck: " + theDeck.size();
     }
 }
